639e332a0e098ff4f940bb34353f1e70e2557bfe
Nimmy Abraham Chandredath <nimmysmail@gmail.com>
2025-08-06 23:59:42 +0300

    update tsconfig

--- tests/addition.spec.ts
+++ tests/addition.spec.ts
@@ -1,15 +1,11 @@
 import { test } from '@playwright/test';
-// import dotenv from 'dotenv';
-// dotenv.config()
-import { baseUrl } from 'globalConfig/constants.js';
 
 test.describe(`Automate the 'Addition' operation of 'Vite App'`, () => {
 
     test(`Should perform 'Addition' of numbers successfully`, async ({ page }) => {
         
         await test.step(`Redirect to the 'Vite App'`, async () => {
-             await page.waitForLoadState('load');
-            await page.goto(`${baseUrl}/`);
+            await page.goto(`/`);
             await page.waitForLoadState('domcontentloaded');
         });
 
